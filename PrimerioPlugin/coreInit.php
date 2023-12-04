0b2ef7511ea899d4d6f31928097055ad73ecdbf6
Lucas Vinicius Oliveira Galindo <lucas.galindo@corp.directweb.br>
2023-12-04 15:56:39 -0300

    inserindo os cores

--- PrimerioPlugin/coreInit.php
+++ PrimerioPlugin/coreInit.php
@@ -1,21 +1,24 @@
 <?php
 
 
-if ( ! defined("WPINC") ){
+if (!defined("WPINC")) {
     die();
 }
 
-define("CORE_INC",dirname(__FILE__) ."/inc/");
-define("CORE_IMG",plugins_url("img/", dirname(__FILE__) ));
-define("CORE_CSS",plugins_url("css/", dirname(__FILE__) ));
-define("CORE_JS",plugins_url("js/", dirname(__FILE__) ));
+define("CORE_INC", dirname(__FILE__) . "/inc/");
+define("CORE_IMG", plugins_url("img/", dirname(__FILE__)));
+define("CORE_CSS", plugins_url("css/", dirname(__FILE__)));
+define("CORE_JS", plugins_url("js/", dirname(__FILE__)));
 
-function np_registerCoreCss() {
-    wp_enqueue_style('core', CORE_CSS . 'index.css', null, time(), 'all' );
+function np_registerCoreCss()
+{
+    wp_enqueue_style('core', CORE_CSS . 'index.css', null, time(), 'all');
 }
 
 add_action("wp_enqueue_scripts", "np_registerCoreCss");
-function np_registerCoreJs() {
+
+function np_registerCoreJs()
+{
     wp_enqueue_script('core', CORE_JS . 'index.js', 'jquery', time(), true);
 }
 
--- PrimerioPlugin/newPlugin.php
+++ PrimerioPlugin/newPlugin.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Plugin Name: Novo Plugin
- * Plugin URI: https://devel2.wp.eduead.com.br/wp-admin/plugins.php
+ * Plugin URI: https://devel2.wp.eduead.com.br/plugins.php
  * Description: Plugin de Funcionalidades para WP
- * Version: 2.0
+ * Version: 2.2
  * Author: Lucas Vinicius
  * Author URI: https://github.com/Lucsv1/Plugins.git
  * Text Domain: np
  */
+
+
 
  if (! defined("WPINC")){
     die();
